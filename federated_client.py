64d6057cf0910e8b24d5f51512c717969d959d56
pavan_gurazada <35609736+pgurazada@users.noreply.github.com>
2024-08-28 11:13:07 +0530

    Create federated_client.py

---
+++ federated_client.py
@@ -0,0 +1,48 @@
+import warnings
+
+import federated_utils
+import flwr as fl
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+
+if __name__ == "__main__":
+    # Load MNIST dataset from https://www.openml.org/d/554
+    (X_train, y_train), (X_test, y_test) = federated_utils.load_mnist()
+
+    # Split train set into 10 partitions and randomly use one for training.
+    partition_id = np.random.choice(10)
+    (X_train, y_train) = federated_utils.partition(X_train, y_train, 10)[partition_id]
+
+    # Create LogisticRegression Model
+    model = LogisticRegression(
+        penalty="l2",
+        max_iter=1,  # local epoch
+        warm_start=True,  # prevent refreshing weights when fitting
+    )
+
+    # Setting initial parameters, akin to model.compile for Keras models
+    federated_utils.set_initial_params(model)
+
+    # Define Flower client
+    class MnistClient(fl.client.NumPyClient):
+        def get_parameters(self, config):  # type: ignore
+            return federated_utils.get_model_parameters(model)
+
+        def fit(self, parameters, config):  # type: ignore
+            federated_utils.set_model_params(model, parameters)
+            # Ignore convergence failure due to low local epochs
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                model.fit(X_train, y_train)
+            print(f"Training finished for round {config['server_round']}")
+            return federated_utils.get_model_parameters(model), len(X_train), {}
+
+        def evaluate(self, parameters, config):  # type: ignore
+            federated_utils.set_model_params(model, parameters)
+            loss = log_loss(y_test, model.predict_proba(X_test))
+            accuracy = model.score(X_test, y_test)
+            return loss, len(X_test), {"accuracy": accuracy}
+
+    # Start Flower client
+    fl.client.start_numpy_client(server_address="0.0.0.0:8080", client=MnistClient())
